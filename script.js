0413078ef898dbc0b5630a25af4a59ed134eb2c9
jmedina0126 <j.medina0126@gmail.com>
2020-12-09 15:39:41 +0000

    Modified form script.js.

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ function div_show() {
     document.getElementById('abc').style.display = "block";
 }
 
-// Validating Empty Field
+/*// Validating Empty Field
 function check_empty() {
     if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
     alert("Fill All Fields !");
     } else {
-    document.getElementById('form').submit();
-    alert("Form Submitted Successfully...");
+        document.getElementById('form').submit();
+        alert("Form Submitted Successfully...");
     }
-}
+}*/
     //Function To Display Popup
     function div_show() {
     document.getElementById('abc').style.display = "block";
