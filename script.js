434c376f78318cf14759ff4c2456a10dccdcb760
Jemar <j.medina0126@gmail.com>
2020-12-26 21:03:21 +0000

    Fixing button
    Fixed the modal not opening with the desktop cta button.

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ hamburger.addEventListener('click', () => {
 });
 
 // Form
+
+// Get DOM Elements
+const modal = document.querySelector('#contactModal');
+const modalBtnD = document.querySelector('#modalBtn-d');
+const modalBtnM = document.querySelector('#modalBtn-m');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtnD.addEventListener('click', openModal);
+modalBtnM.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
